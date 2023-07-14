55a5b10d3697cf25ecd8c4e8663bbe13cab9cfa6
bahdfella <timothyikejiaku@gmail.com>
2023-07-14 02:20:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - a program that prints using put
+ * return (0) - success
+ **/
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
